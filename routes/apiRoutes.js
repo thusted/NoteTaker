37b903155166a4408933a64ec372146e137f218b
Tiana Husted <tiana.husted@gmail.com>
2020-08-30 18:11:17 -0700

    Adding api routes for getting all stored notes, post new note, and delete note

---
+++ routes/apiRoutes.js
@@ -0,0 +1,18 @@
+const storedNotes = require("../db/db.json");
+
+module.exports = function(app) {
+  app.get("/api/notes", function(req, res) {
+    res.json(storedNotes);
+  });
+
+  app.post("/api/notes", function(req, res) {
+    storedNotes.push(req.body);
+    res.json(storedNotes);
+  });
+  
+  app.delete("/api/notes/:id", function(req, res) {
+    const noteId = req.params.id;
+    
+  })
+    
+}
